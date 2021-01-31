8917560e154309a691d42c97c0f3a96b7da79f88
DSarceno <dsarceno68@gmail.com>
2021-01-30 18:25:24 -0600

    Proy. Parciales. Cap 1 Sec 1

--- Parciales1/Proyecto/delShitFiles.py
+++ Parciales1/Proyecto/delShitFiles.py
@@ -5,5 +5,6 @@ name = sys.argv[1]
 
 subprocess.call(['rm', name + '.aux'])
 subprocess.call(['rm', name + '.log'])
+subprocess.call(['rm', name + '.toc'])
 subprocess.call(['rm', name + '.out'])
 subprocess.call(['rm', name + '.synctex.gz'])
