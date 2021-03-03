2e265843895b404b7cd0ef9c8acfadff7e8d7844
DSarceno <dsarceno68@gmail.com>
2021-03-02 22:59:09 -0600

    RD T2

---
+++ ReduccionDatos/tarea2.R
@@ -0,0 +1,22 @@
+# 02.03.2021
+# Diego Sarceño (dsarceno68@gmail.com)
+#
+# 
+# Instrucciones de Compilación:
+# Rscript tarea2.R
+#
+# Tarea 2 Lab Reducción de Datos
+# Creada simplemente por ayuda y ahorro de tiempo el tratar datos
+#
+# 
+
+# PROBLEMA 4.2
+datos1 <- c(9.9,9.6,9.5,9.7,9.8)
+
+mean(datos1)
+sd(datos1)
+
+datos2 <- c(86,85,84,89,85,89,87,85,82,85)
+
+mean(datos2)
+sd(datos2)
