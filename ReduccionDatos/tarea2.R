3a963a1d595d72ac9fea95df4e061a366d38d037
DSarceno <dsarceno68@gmail.com>
2021-03-04 21:00:03 -0600

    T2RD

--- ReduccionDatos/tarea2.R
+++ ReduccionDatos/tarea2.R
@@ -20,3 +20,13 @@ datos2 <- c(86,85,84,89,85,89,87,85,82,85)
 
 mean(datos2)
 sd(datos2)
+
+
+# PROBLEMA 4.22
+datosG <- c(977.81,982.34,979.09,981.32,984.29)
+
+mean(datosG)
+std <- sd(datosG)
+
+print(std)
+print(std/5)
