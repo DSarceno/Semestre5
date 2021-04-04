9cb2b78f9b7c39f0adbd2e4b607019e73659840f
DSarceno <dsarceno68@gmail.com>
2021-04-04 00:26:40 -0600

    Practica1 Casi

--- Programación2/Trabajos/Practica1/Sessions/usersManagement/models.py
+++ Programación2/Trabajos/Practica1/Sessions/usersManagement/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
+class usuarios(models.Model):
     email = models.EmailField()
+    nickname = models.CharField(max_length = 10)
     nombre = models.CharField(max_length = 15)
     apellido = models.CharField(max_length = 20)
+    sexo = models.CharField(max_length = 9)
+    carrera = models.CharField(max_length = 30)
     password = models.CharField(max_length = 15)
--- Programación2/Trabajos/Practica1/Sessions/usersManagement/views.py
+++ Programación2/Trabajos/Practica1/Sessions/usersManagement/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from usersManagement.models import Users
+from usersManagement.models import usuarios
 
 # Create your views here.
 
@@ -9,15 +9,26 @@ def registro(request):
         Email = request.POST['Email']
         Nombre = request.POST['Nombre']
         Apellido = request.POST['Apellido']
+        Nickname = request.POST['Nickname']
+        Sexo = request.POST['sexo']
+        Carrera = request.POST['Carrera']
         Contra = request.POST['Password']
         ConfContra = request.POST['ConfirmPassword']
 
-        if (Contra == ConfContra) and (Users.objects.filter(nombre__icontains = Email) == []):
-            user = Users(email = Email, nombre = Nombre, apellido = Apellido, password = Contra)
-            user.save()
-
-
-            return render(request, 'gracias.html')
+        print(Email)
+        print(Nickname)
+        print(Sexo)
+        print(Carrera)
+        print(Contra)
+        if (Contra != ConfContra):
+            return render(request, 'signin.html')
+        else:
+            if ((usuarios.objects.filter(email__icontains = Email) != []) or (usuarios.objects.filter(nickname__icontains = Nickname) != [])):
+                return render(request, 'signin.html')
+            else:
+                user = usuarios(email = Email, nickname = Nickname, nombre = Nombre, apellido = Apellido, sexo = Sexo, carrera = Carrera, password = Contra)
+                user.save()
+                return render(request, 'gracias.html')
     return render(request, 'signin.html')
 
 def registrado(request):
