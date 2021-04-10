d8d32562bfe6f00b05dbe5907a4d3a8e2626a233
DSarceno <dsarceno68@gmail.com>
2021-04-10 00:16:00 -0600

    Fuera taller

--- Taller1/ApuntesDeClase/delShitFiles.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-name = sys.argv[1]
-
-subprocess.call(['rm', name + '.aux'])
-subprocess.call(['rm', name + '.log'])
-subprocess.call(['rm', name + '.out'])
-subprocess.call(['rm', name + '.synctex.gz'])
