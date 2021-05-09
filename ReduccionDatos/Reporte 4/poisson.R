8c3b35a8554f2541f1a25594e061bb29cf08dd30
DSarceno <dsarceno68@gmail.com>
2021-05-09 16:42:42 -0600

    Reporte 4 Archivos comentados

--- ReduccionDatos/Reporte 4/poisson.R
+++ ReduccionDatos/Reporte 4/poisson.R
@@ -1,3 +1,33 @@
+#    2021-05-09
+#    poisson.R
+#    Diego Sarceño (dsarceno68@gmail.com)
+
+#    Análisis bajo la distribución de Poisson.
+
+#    Codificación del texto: UTF8
+#    Compiladores probados: R (Ubuntu 20.04 Linux) 4.0.3
+#    Instrucciones de compilación: no requiere nada más
+#    Rscript poisson.R
+
+#    Copyright (C) 2021
+#    D. R. Sarceño Ramírez
+#    dsarceno68@gmail.com
+#
+#
+#    This program is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
+# PROGRAM poisson
 datos_10 <- scan('10exp.dat')
 
 
